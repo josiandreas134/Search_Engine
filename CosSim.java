ef6d7c886a4edb69f18733c030100db949942bc8
riwandy <riwandy@outlook.com>
2020-05-12 00:25:58 +0700

    fix CosSim when inverted index return null

--- CosSim.java
+++ CosSim.java
@@ -54,40 +54,39 @@ public class CosSim {
         for(int i=0; i<q.size(); i++) {
             // HashMap<String, Integer> freq = new HashMap<String, Integer>();
             // List pages with title that contains word(s) from query
-            String title_pagelist_str = new String(titleInverted.get((q.get(i)).getBytes()));
-            Vector<String> title_pagelist = new Vector<String>(Arrays.asList(title_pagelist_str.split("\\s+")));
+            
             String lastDoc = "";
-            for(int j=0; j<title_pagelist.size(); j++) {
-                if(title_pagelist.get(j).length() > 3 && title_pagelist.get(j).substring(0,3).equals("doc")) {
-                    lastDoc = title_pagelist.get(j).substring(3);
-                    cossim_u.putIfAbsent(lastDoc, 0);
-                }
-                else {
-                    cossim_u.put(lastDoc, cossim_u.get(lastDoc)+2);
+            if(titleInverted.get((q.get(i)).getBytes()) != null) {
+                String title_pagelist_str = new String(titleInverted.get((q.get(i)).getBytes()));
+                Vector<String> title_pagelist = new Vector<String>(Arrays.asList(title_pagelist_str.split("\\s+")));
+                lastDoc = "";
+                for(int j=0; j<title_pagelist.size(); j++) {
+                    if(title_pagelist.get(j).length() > 3 && title_pagelist.get(j).substring(0,3).equals("doc")) {
+                        lastDoc = title_pagelist.get(j).substring(3);
+                        cossim_u.putIfAbsent(lastDoc, 0);
+                    }
+                    else {
+                        cossim_u.put(lastDoc, cossim_u.get(lastDoc)+2);
+                    }
                 }
             }
             
             // List pages with content that contains word(s) from query
-            String content_pagelist_str = new String(contentInverted.get((q.get(i)).getBytes()));
-            Vector<String> content_pagelist = new Vector<String>(Arrays.asList(content_pagelist_str.split("\\s+")));
-            lastDoc = "";
-            for(int j=0; j<content_pagelist.size(); j++) {
-                // System.out.println(content_pagelist.get(j));
-                if(content_pagelist.get(j).length() > 3 && content_pagelist.get(j).substring(0,3).equals("doc")) {
-                    lastDoc = content_pagelist. get(j).substring(3);
-                    cossim_u.putIfAbsent(lastDoc, 0);
-                }
-                else {
-                    cossim_u.put(lastDoc, cossim_u.get(lastDoc)+1);
+            if(contentInverted.get((q.get(i)).getBytes()) != null) {
+                String content_pagelist_str = new String(contentInverted.get((q.get(i)).getBytes()));
+                Vector<String> content_pagelist = new Vector<String>(Arrays.asList(content_pagelist_str.split("\\s+")));
+                lastDoc = "";
+                for(int j=0; j<content_pagelist.size(); j++) {
+                    // System.out.println(content_pagelist.get(j));
+                    if(content_pagelist.get(j).length() > 3 && content_pagelist.get(j).substring(0,3).equals("doc")) {
+                        lastDoc = content_pagelist. get(j).substring(3);
+                        cossim_u.putIfAbsent(lastDoc, 0);
+                    }
+                    else {
+                        cossim_u.put(lastDoc, cossim_u.get(lastDoc)+1);
+                    }
                 }
             }
-
-            // for (String key : freq.keySet()) {
-            //     cossim_u.putIfAbsent(key, 1);
-            //     cossim_u.put(key, cossim_u.get(key)+freq.get(key));
-            // }
-
-            // System.out.println("freq "+q.get(i));
         }
         for (String key : cossim_u.keySet()) {
             String fwd_str = new String(forwardIndex.get(key.getBytes()));
@@ -102,32 +101,34 @@ public class CosSim {
         return cossim;
     }
 
-    // public static void main(String[] args) {
-    //     try{
-    //         RocksDB.loadLibrary();
-    //         Options options = new Options();
-    //         options.setCreateIfMissing(true);
-    //         RocksDB document = RocksDB.open(options, "db/document");
-    //         RocksDB invertedDocument = RocksDB.open(options, "db/invertedDocument");
-    //         RocksDB word = RocksDB.open(options, "db/word");
-    //         RocksDB invertedWord = RocksDB.open(options, "db/invertedWord");
-    //         RocksDB forwardIndex = RocksDB.open(options, "db/forwardIndex");
-    //         RocksDB titleInverted = RocksDB.open(options, "db/titleInverted");
-    //         RocksDB contentInverted = RocksDB.open(options, "db/contentInverted");
+    public static void main(String[] args) {
+        try{
+            RocksDB.loadLibrary();
+            Options options = new Options();
+            options.setCreateIfMissing(true);
+            RocksDB document = RocksDB.open(options, "db/document");
+            RocksDB invertedDocument = RocksDB.open(options, "db/invertedDocument");
+            RocksDB word = RocksDB.open(options, "db/word");
+            RocksDB invertedWord = RocksDB.open(options, "db/invertedWord");
+            RocksDB forwardIndex = RocksDB.open(options, "db/forwardIndex");
+            RocksDB titleInverted = RocksDB.open(options, "db/titleInverted");
+            RocksDB contentInverted = RocksDB.open(options, "db/contentInverted");
 
-    //         System.out.println(new String(titleInverted.get("1".getBytes())));
-    //         System.out.println(new String(titleInverted.get("2".getBytes())));
-    //         System.out.println(new String(titleInverted.get("3".getBytes())));
-    //         System.out.println(new String(contentInverted.get("1".getBytes())));
-    //         System.out.println(new String(contentInverted.get("2".getBytes())));
-    //         System.out.println(new String(contentInverted.get("3".getBytes())));
+            System.out.println(new String(titleInverted.get("1".getBytes())));
+            System.out.println(new String(titleInverted.get("2".getBytes())));
+            System.out.println(new String(titleInverted.get("3".getBytes())));
+            System.out.println(new String(contentInverted.get("1".getBytes())));
+            System.out.println(new String(contentInverted.get("2".getBytes())));
+            System.out.println(new String(contentInverted.get("3".getBytes())));
+            // System.out.println(new String(word.get("HKUST".getBytes())));
+            // System.out.println(new String(word.get("SCHOOL".getBytes())));
 
-    //         CosSim c= new CosSim("HKUST COMPUTER", word, titleInverted, contentInverted, forwardIndex);
-    //         System.out.println(c.getPages(0.2));
-    //     }
-    //     catch(RocksDBException e)
-    //     {
-    //         System.err.println(e.toString());
-    //     }
-    // }
+            CosSim c= new CosSim("HKUST MAKAN", word, titleInverted, contentInverted, forwardIndex);
+            System.out.println(c.getPages(0.2));
+        }
+        catch(RocksDBException e)
+        {
+            System.err.println(e.toString());
+        }
+    }
 }
